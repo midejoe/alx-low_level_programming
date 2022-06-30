f6e6aab5c7a41ffb36871ac44d3d7847424c6a74
joseph <midejoseph24@gmail.com>
2022-06-30 19:58:13 +0000

    Add check size script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 
 /**
 * main - Entry point
-* 
 * Return: Always 0 (Success)
 */
 int main(void)
@@ -12,11 +11,10 @@ int main(void)
 	long long int c;
 	float d;
 	char e;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof( e ));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof( a ));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof( b ));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof( c ));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof( d ));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
 	return (0);
-}	
+} 	
