bcbe334a57e6b6b638bf05a8d8812240ed03d425
joseph <midejoseph24@gmail.com>
2022-07-06 00:33:29 +0000

    Add header file and last digit code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,17 +10,17 @@
  */
 int print_last_digit(int n)
 {
-  int l;
-
-  l = n % 10;
-  if (l < 0)
-    {
-      _putchar(-l + 48);
-      return (-l);
-    }
-  else
-    {
-      _putchar(l + 48);
-      return (l);
-    }
+	int l;
+	
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
 }
