dacccac06970d0fd4d1044dabaaae5b9a1b9bdba
joseph <midejoseph24@gmail.com>
2022-07-07 22:49:04 +0000

    Add print square code

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * print_square - function to print a square
+ *   * @size: takes in the size of the square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			putchar('#');
+		putchar('\n');
+	}
+}
